bf2c55cba3a684b9bd1aa26cd2424de016aa51b0
La_Professor_De_BellaCiao <wofford.codes@gmail.com>
2022-10-22 20:34:39 -0500

    kels:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :calendars
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
